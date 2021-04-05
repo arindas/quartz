92cc4d3c1966bdf72b5b8319622c9802cd1b104d
Arindam Das <dasarindam.mails@gmail.com>
2021-04-05 22:18:24 +0530

    Added operations on vector for tracing rays

--- include/quartz/vec3.h
+++ include/quartz/vec3.h
@@ -27,7 +27,64 @@ namespace quartz
         return out << v.x << " " << v.y << " " << v.z;
     }
 
+    template <class T>
+    inline vec3<T> operator+(const vec3<T> &u, const vec3<T> &v)
+    {
+        return vec3<T>(u.x + v.x, u.y + v.y, u.z + v.z);
+    }
+
+    template <class T>
+    inline vec3<T> operator-(const vec3<T> &u, const vec3<T> &v)
+    {
+        return vec3<T>(u.x + v.x, u.y + v.y, u.z + v.z);
+    }
+
+    template <class T>
+    inline vec3<T> operator*(const vec3<T> &u, const vec3<T> &v)
+    {
+        return vec3<T>(u.x * v.x, u.y * v.y, u.z * v.z);
+    }
+
+    template <class T>
+    inline vec3<T> operator*(const vec3<T> &u, T t)
+    {
+        return vec3<T>(u.x * t, u.y * t, u.z * t);
+    }
+
+    template <class T>
+    inline vec3<T> operator*(T t, const vec3<T> &u)
+    {
+        return u * t;
+    }
+
+    template <class T>
+    inline vec3<T> operator/(const vec3<T> &u, T t)
+    {
+        return (1 / t) * u;
+    }
+
+    template <class T>
+    inline T dot(const vec3<T> &u, const vec3<T> &v)
+    {
+        return u.x * v.x + u.y + v.y + u.z * v.z;
+    }
+
+    template <class T>
+    inline vec3<T> cross(const vec3<T> &u, const vec3<T> &v)
+    {
+        return vec3<T>(u.y * v.z - u.z * v.y,
+                       u.x * v.z - u.z * v.x,
+                       u.x * v.y - u.y * v.x);
+    }
+
+    template <class T>
+    inline vec3<T> unit_vector(const vec3<T> u) {
+        return u / u.length();
+    }
+
     using point3 = vec3<double>;
     using color = vec3<double>;
 };
+
+
 #endif
