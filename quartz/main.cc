124f7d70deb42c33e5be7fed3c2fb1e002d93c20
Arindam Das <dasarindam.mails@gmail.com>
2021-04-06 18:43:07 +0530

    Added basic sphere hit detection.

--- quartz/main.cc
+++ quartz/main.cc
@@ -3,10 +3,43 @@
 #include <quartz/color.h>
 #include <quartz/ray.h>
 
+// hit_sphere: function to determine whether a ray hits a sphere centred at the given point
+// with the given radius. The equation for a point intersecting on the surface of a sphere
+// is given as follows:
+//      t*t * dot(b_, b_) + 2*t * dot(b, A_ - C_) + dot(A_ - C_, A_ - C_) - r*r = 0
+// where
+//  the ray is denoted by P(t) = A_ + dot(b_, t)
+//  the sphere is centered at C_ with radius r
+//
+// Comparing this equation with at*T + b*t + c, i.e a quadratic equation in t, we have:
+//      a = dot(b_, b_)
+//      b = 2 * dot(b_, A_ - C_)
+//      c = dot(A_ - C_, A_ - C_) - r*r
+//
+// We only need to check whether for the given coefficients for the equation in t, solutions
+// exist or not. We do that by checking whether the discriminant is greater than 0.
+// i.e b*b - 4*a*c > 0
+bool hit_sphere(const quartz::point3 &center, double radius, const quartz::ray &r)
+{
+    quartz::vec3<double> oc = r.origin - center; // A_ - C_
+
+    auto a = quartz::dot(r.direction, r.direction); // dot(b_, b_)
+    auto b = 2.0 * quartz::dot(r.direction, oc);    // 2 * dot(b_, A_ - C_)
+    auto c = quartz::dot(oc, oc) - radius * radius; // dot(A_ - C_, A_ - C_) - r*r
+
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 // ray_color: temporary ray to color mapper for testing. Produces a blue to
 // white gradient from top to bottom.
 quartz::color ray_color(const quartz::ray &r)
 {
+    if (hit_sphere(quartz::point3(0, 0, -1), 0.5, r))
+    {
+        return quartz::color(1, 0, 0);
+    }
+
     // obtain unit vector direction for given ray
     quartz::vec3<double> unit_direction = quartz::unit_vector(r.direction);
 
@@ -39,15 +72,18 @@ int main(int, char **)
 
     // Render
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << " " << image_height << "\n255\n";
 
-    for(int j = image_height - 1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; --j)
+    {
         std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
-        for(int i = 0; i < image_width; i++) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            
-            quartz::ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        for (int i = 0; i < image_width; i++)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+
+            quartz::ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             quartz::color pixel_color = ray_color(r);
 
             quartz::write_color(std::cout, pixel_color);
